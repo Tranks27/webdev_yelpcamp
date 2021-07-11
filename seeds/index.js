35268c009e155232b9bb884f1c0c925c8af298fb
tranks_winDesktop <tranksntb@gmail.com>
2021-07-11 22:07:54 +1000

    add basic styles to pages

--- seeds/index.js
+++ seeds/index.js
@@ -24,9 +24,13 @@ const seedDB = async () =>{
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price =  Math.floor(Math.random()*30) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo ipsam autem inventore quos fuga esse! Numquam optio modi, placeat consequuntur tempore impedit, non nihil exercitationem laudantium quod doloribus repudiandae? Dolorum?",
+            price 
         })
         await camp.save();
     }
